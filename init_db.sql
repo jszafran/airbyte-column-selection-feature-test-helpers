25a68ab5a6da098e303a082f52ab7b12c611a068
jszafran <jszafran.pv@gmail.com>
2023-06-17 17:49:52 +0200

    Add docker-compose and init db script for PostgreSQL 15.

---
+++ init_db.sql
@@ -0,0 +1,23 @@
+-- create schemas
+CREATE SCHEMA source;
+CREATE SCHEMA destination;
+
+-- create source tables
+CREATE TABLE source.table_a (
+    id serial primary key,
+    column_1 varchar,
+    column_2 varchar,
+    column_3 varchar
+);
+
+-- create replication slot & publication
+SELECT pg_create_logical_replication_slot('airbyte_slot', 'pgoutput');
+
+CREATE PUBLICATION airbyte_publication_full FOR TABLE source.table_a;
+
+-- add initial records
+INSERT INTO source.table_a (column_1, column_2, column_3)
+VALUES
+('foo1', 'foo2', 'foo3'),
+('bar1', 'bar2', 'bar3'),
+('baz1', 'baz2', 'baz3');
